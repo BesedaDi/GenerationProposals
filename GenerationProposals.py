3ded476a1c955a99d57adfb2f36befc2e213904b
BesedaDi <dianabesedina223@gmail.com>
2020-03-19 18:21:00 +0700

    start

--- GenerationProposals.py
+++ GenerationProposals.py
@@ -6,3 +6,18 @@
 
 """
 import random
+
+symbols = ['!','@','#','$','%']
+f = open('input.txt','r', encoding='utf8')
+text = f.read()
+for symbol in symbols:
+    if symbol in text:
+        text = text.replace(symbol,'')
+import string
+import re
+
+
+pat = "\text+([{}]+)".format(re.escape(string.punctuation))
+res = re.sub("\s{2,}", " ", re.sub(pat, r"\1", text))
+text = res.replace('\n', ' ')
+print(text)
