90815dffa2e82796bec56801c5ccecd192f2280b
BesedaDi <dianabesedina223@gmail.com>
2020-03-20 20:46:28 +0700

    ready(:)

--- GenerationProposals.py
+++ GenerationProposals.py
@@ -2,12 +2,10 @@
 ###
 """ Case-study #6 Генерация предложения
 Разработчики:
-Беседина Д.В., Сецков М.
+Беседина Д.В.(40%), Сецков М.(60%)
 
 """
 import random
-
-
 
 
 def word():
@@ -35,6 +33,7 @@ def word():
     word = list(text1.split())
     return word
 
+
 source_words = word()
 list_start = []
 for word in source_words:
@@ -50,14 +49,14 @@ for word in source_words:
     listLinks.append(word)
     listMore = []
     if index != len(source_words) - 1:
-        listMore.append(source_words[index+1])
+        listMore.append(source_words[index + 1])
     wordAmount = source_words.count(word)
     if wordAmount > 1:
         start = index + 1
         end = len(source_words)
-        for i in range(wordAmount-1):
+        for i in range(wordAmount - 1):
             index = source_words.index(word, start, end)
-            listMore.append(source_words[index+1])
+            listMore.append(source_words[index + 1])
             if source_words[index] != source_words[-1]:
                 start = index + 1
             else:
@@ -68,21 +67,21 @@ listLinks = listLinks[:-2]
 n = int(input('Сколько предложений нужно сгенерировать из исходного текста? '))
 amount_startWords = len(list_start)
 for i in range(n):
-    firstWord = random.randint(0, amount_startWords-1)
+    firstWord = random.randint(0, amount_startWords - 1)
     first_word = list_start[firstWord]
     sent = first_word + ' '
     indexFirst = listLinks.index(first_word)
-    for i in range(random.randint(5,20)):
-        if len(listLinks[indexFirst+1]) > 1:
-            lenght = len(listLinks[indexFirst+1])
+    for i in range(random.randint(5, 20)):
+        if len(listLinks[indexFirst + 1]) > 1:
+            lenght = len(listLinks[indexFirst + 1])
             number = random.randint(0, lenght)
-            secWord = (listLinks[indexFirst+1])[number-1]
+            secWord = (listLinks[indexFirst + 1])[number - 1]
             sent += str(secWord) + ' '
             if (str(secWord))[-1] == '.':
                 break
             indexFirst = listLinks.index(str(secWord))
         else:
-            secWord = listLinks[indexFirst+1]
+            secWord = listLinks[indexFirst + 1]
             sent += (str(secWord))[2:-2] + ' '
             if ((str(secWord))[2:-2])[-1] == '.':
                 break
@@ -92,7 +91,3 @@ for i in range(n):
         print('.', end=' ')
     else:
         print(sent, end='')
-
-
-
-
